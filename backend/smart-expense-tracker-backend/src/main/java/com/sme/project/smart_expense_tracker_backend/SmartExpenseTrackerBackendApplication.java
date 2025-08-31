4242624b4611948041ea60bf0228757213fa0b82
saikumartalam11 <saikumar.talam.11@gmail.com>
2025-08-31 21:46:45 +0530

    Initial project structure with backend, frontend, ai-service, and docs

---
+++ backend/smart-expense-tracker-backend/src/main/java/com/sme/project/smart_expense_tracker_backend/SmartExpenseTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sme.project.smart_expense_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseTrackerBackendApplication.class, args);
+	}
+
+}
